3ef1384abef863c5bd41ca75d1dd952a724d33fe
programmerasif <mdasifkhan3757@gmail.com>
2023-09-26 19:04:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AllUsers from './components/AllUsers/AllUsers'
+
+function App() {
+ 
+
+  return (
+    <>
+    this is home 
+    <AllUsers />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AllUsers/AllUsers.jsx
@@ -0,0 +1,12 @@
+import ima from '../../assets/rsz_1close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background.jpg'
+const AllUsers = () => {
+    return (
+        <div className="grid grid-cols-2 w-[80%] mx-auto">
+            <div className="flex justify-between items-center">
+                <img src={ima} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default AllUsers;
